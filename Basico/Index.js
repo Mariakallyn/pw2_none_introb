894abd05846a5e155a7587d453d34aa54686ccf9
Mariakallyn <maria.silva5160@etec.sp.gov.br>
2024-02-27 20:45:19 -0300

    aula2702 -Pacotes Externos Chalk

--- Basico/Index.js
+++ Basico/Index.js
@@ -1,1 +1,43 @@
-console.log("Olá Mundo")
+/*const chalk = require('chalk')
+
+function calculadora(n1, n2, op){
+
+    if(op === "+")
+    {
+        return( chalk.bgBlue.white( ${n1} ${op} ${n2} = ${(n1+n2)} `)
+    }else
+    if (op === "-") {
+        
+        return(chalk.bggreen.black`(${n1} ${op} ${n2} = ${(n1-n2)}) `)
+    }else
+    if(op === "*"){
+
+        return(chalk .bgmangeta`(${n1} ${op} ${n2} = ${(n1*n2)}) `)
+    }else
+    if(op === "/"){
+
+        return('(${n1} ${op} ${n2} = ${(n1/n2)}) `)
+    }else {
+
+        return(chalk.bgRed.Yellow('invalid operation`))
+    }
+
+}
+   
+
+
+console.log(calculadora(135, 61, "+"))
+console.log(calculadora (135, 61, "-"))
+console.log(calculadora(15, 5, "*"))
+console.log(calculadora(144, 12, "/"))
+console.log(calculadora(32, 6, "|"))*/
+
+switch (op) {
+    case '+':
+        return (chalk.bgBlue.white( ${n1} ${op} ${n2} = ${(n1+n2)} `)
+     '-' :
+     case '-':
+        return(chalk.gbGreen.black( ${n1} ${op} ${n2} = ${(n1+n2)} `)
+       case '/':
+        return (chalk.bgYellow.)
+}
